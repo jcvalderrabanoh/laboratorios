edfd055bdcc21f1359e213f90779244c9c9626cd
Juan Carlos Valderrábano Hernández <jcvalderrabano@getpulpo.com>
2022-10-01 11:25:04 -0500

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+const puppeteer = require('puppeteer');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello() {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://blog.risingstack.com', {
+      waitUntil: 'networkidle0',
+    });
+    const pdf = await page.pdf({ format: 'A4' });
+
+    await browser.close();
+    return pdf;
   }
 }
